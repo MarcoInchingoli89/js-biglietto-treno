af403f219b6e33882691168b1adffa2c300e7e05
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-06 15:25:47 +0200

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+/* Calcolo costo biglietto del treno */
+
+/* Istruzioni */
+
+// Il programma chiede all'utente i chilometi che vuole percorrere
+// Il programma chiede l'età del passeggero
+
+/* Calcolo prezzo biglietto */
+
+// Il prezzo è definito in base ai km (0.21 € al km)
+// Applica il 20% di sconto ai minorenni
+// Applica il 40% di sconto agli over 65
+// L'output del prezzo finale va scritto in forma umana
