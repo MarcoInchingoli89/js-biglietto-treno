dd36b93dfc8c93412d2d48c3d630386f86c82d76
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-06 17:08:12 +0200

    Add Fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,16 @@
 /* Istruzioni */
 
 // Il programma chiede all'utente i chilometri che vuole percorrere
-const kmTraveled = Number (prompt ('Quanti chilometri vuoi percorrere?'));
+const kmTraveled = prompt ('Quanti chilometri vuoi percorrere?');
 console.log(kmTraveled);
 // Il programma chiede l'età del passeggero
-const passengerAge = Number (prompt ('Qual`è la tua età?'));
+const passengerAge = prompt ('Qual`è la tua età?');
 console.log(passengerAge);
 
 /* Calcolo prezzo biglietto */
 
 // Calcola il prezzo in base ai km (0.21 € al km)
-const ticketPrice = Number ((kmTraveled * 0.21));
+const ticketPrice = (kmTraveled * 0.21);
 // Se il passeggero è minorenne applica il 20% di sconto
 if (passengerAge < 18) {
     const underageDiscount = ((passengerAge * 20) /100);
