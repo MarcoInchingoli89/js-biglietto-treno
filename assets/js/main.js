25b8933e59fa90e9ee46f45301ef309f5a283c96
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-06 16:16:21 +0200

    Add if else if else

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,19 +6,26 @@
 const kmTraveled = prompt ('Quanti chilometri vuoi percorrere?');
 console.log(kmTraveled);
 // Il programma chiede l'età del passeggero
-const passengerAge = prompt ('Qual`è la tua età?')
-console.log(passengerAge)
+const passengerAge = prompt ('Qual`è la tua età?');
+console.log(passengerAge);
+
 /* Calcolo prezzo biglietto */
 
-// Il prezzo è definito in base ai km (0.21 € al km)
-const ticketPrice = (kmTraveled * 0.21)
-console.log(ticketPrice)
+// Calcolsa il prezzo in base ai km (0.21 € al km)
+const ticketPrice = (kmTraveled * 0.21);
 // Applica il 20% di sconto ai minorenni
-const underageDiscount = ((passengerAge * 20) /100)
-const underageTicketPrice = (ticketPrice - underageDiscount)
-console.log(underageTicketPrice)
+if (passengerAge < 18) {
+    const underageDiscount = ((passengerAge * 20) /100);
+    const underageTicketPrice = (ticketPrice - underageDiscount);
+    console.log(underageTicketPrice);
 // Applica il 40% di sconto agli over 65
-const over65Discount = ((passengerAge * 40) /100)
-const over65TicketPrice = (ticketPrice - over65Discount)
-console.log(over65TicketPrice)
+} else if (passengerAge > 65) {
+    const over65Discount = ((passengerAge * 40) /100);
+    const over65TicketPrice = (ticketPrice - over65Discount);
+    console.log(over65TicketPrice);
+// Mostra il prezzo del biglietto non scontato
+} else {
+    console.log(ticketPrice);
+}
+
 // L'output del prezzo finale va scritto in forma umana
