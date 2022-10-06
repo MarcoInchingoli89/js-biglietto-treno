2092d72df8ff92a9830c6ac52c3e5f201254db0c
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-06 17:00:49 +0200

    Add EUR Currency Final Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,29 +3,27 @@
 /* Istruzioni */
 
 // Il programma chiede all'utente i chilometri che vuole percorrere
-const kmTraveled = prompt ('Quanti chilometri vuoi percorrere?');
+const kmTraveled = Number (prompt ('Quanti chilometri vuoi percorrere?'));
 console.log(kmTraveled);
 // Il programma chiede l'età del passeggero
-const passengerAge = prompt ('Qual`è la tua età?');
+const passengerAge = Number (prompt ('Qual`è la tua età?'));
 console.log(passengerAge);
 
 /* Calcolo prezzo biglietto */
 
-// Calcolsa il prezzo in base ai km (0.21 € al km)
-const ticketPrice = (kmTraveled * 0.21);
-// Applica il 20% di sconto ai minorenni
+// Calcola il prezzo in base ai km (0.21 € al km)
+const ticketPrice = Number ((kmTraveled * 0.21));
+// Se il passeggero è minorenne applica il 20% di sconto
 if (passengerAge < 18) {
     const underageDiscount = ((passengerAge * 20) /100);
     const underageTicketPrice = (ticketPrice - underageDiscount);
-    console.log(underageTicketPrice);
-// Applica il 40% di sconto agli over 65
+    console.log(new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(underageTicketPrice));
+// Se il passeggero ha più di 65 anni applica il 40% di sconto
 } else if (passengerAge > 65) {
     const over65Discount = ((passengerAge * 40) /100);
     const over65TicketPrice = (ticketPrice - over65Discount);
-    console.log(over65TicketPrice);
-// Mostra il prezzo del biglietto non scontato
+    console.log(new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(over65TicketPrice));
+// Altrimenti non applicare nessuno sconto
 } else {
-    console.log(ticketPrice);
+    console.log(new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(ticketPrice));
 }
-
-// L'output del prezzo finale va scritto in forma umana
