b8dcad4bb5da453fbbca3861f3c762f420836a3b
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-06 15:57:17 +0200

    Add Instructions Price and Discount

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,23 @@
 
 /* Istruzioni */
 
-// Il programma chiede all'utente i chilometi che vuole percorrere
+// Il programma chiede all'utente i chilometri che vuole percorrere
+const kmTraveled = prompt ('Quanti chilometri vuoi percorrere?');
+console.log(kmTraveled);
 // Il programma chiede l'età del passeggero
-
+const passengerAge = prompt ('Qual`è la tua età?')
+console.log(passengerAge)
 /* Calcolo prezzo biglietto */
 
 // Il prezzo è definito in base ai km (0.21 € al km)
+const ticketPrice = (kmTraveled * 0.21)
+console.log(ticketPrice)
 // Applica il 20% di sconto ai minorenni
+const underageDiscount = ((passengerAge * 20) /100)
+const underageTicketPrice = (ticketPrice - underageDiscount)
+console.log(underageTicketPrice)
 // Applica il 40% di sconto agli over 65
+const over65Discount = ((passengerAge * 40) /100)
+const over65TicketPrice = (ticketPrice - over65Discount)
+console.log(over65TicketPrice)
 // L'output del prezzo finale va scritto in forma umana
